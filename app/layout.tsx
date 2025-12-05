e1abfa7266c90f034bfeefa305e695e2003b911f
namit2111 <namitjain2111@gmail.com>
2025-12-06 00:00:14 +0530

    updated favicn

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     images: ['https://easytoolstown.com/og-image.png'],
   },
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🛠️</text></svg>",
+    icon: '/favicon.svg',
   },
 };
 
